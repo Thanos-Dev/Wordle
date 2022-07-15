b768ab05b366d9027d2f1131d7fec8d3edb729c0
Thanos <83920089+Thanos-Dev@users.noreply.github.com>
2022-07-15 22:01:55 +0300

    Expanded wordle game class

--- classes/wordleGame.js
+++ classes/wordleGame.js
@@ -1,7 +1,21 @@
-class WordleGame {
-  constructor(client, correctWord) {
+const {MessageEmbed} = require("discord.js");
 
+class WordleGame {
+  constructor(client, correctWord, user) {
+    this.client = client
+    this.correctWord = correctWord
+    this.user = user
   }
+
+  validateWord(guess) {
+    if (guess.length != 5) return "Your guess must be 5 letters long!"
+    if (/^[a-z]+$/i.test(str).test(guess)) return "Your guess must only contain letters!"
+
+    const embed = new MessageEmbed()
+
+    return embed
+  }
+
 }
 
 export default WordleGame
