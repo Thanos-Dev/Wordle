c1da3cfd613057570eb92dabaaf2d9efb57b25fd
Thanos <83920089+Thanos-Dev@users.noreply.github.com>
2022-07-17 00:41:45 +0300

    Began fleshing out WordleGame class

--- classes/wordleGame.js
+++ classes/wordleGame.js
@@ -1,17 +1,31 @@
 const { MessageEmbed } = require("discord.js");
 
 class WordleGame {
-  constructor(client, correctWord, interaction) {
+  constructor(client, correctWord, interaction, baseGuesses) {
     this.client = client
     this.correctWord = correctWord
     this.user = interaction.member
+    this.guesses = baseGuesses
+
+    this.embed = new MessageEmbed() //constantly add new fields to the embed with different words to give feedback
+      .setTitle("Wordle Game Progress")
+      .setColor("GREEN")
+      
+  }
+
+  validateLetter(char) {
+    if (this.correctWord.includes(char)) return `:green_square:`
+
+    return `:black_large_square:`
   }
 
   validateWord(guess) {
     if (guess.length != 5) return "Your guess must be 5 letters long!"
     if (/^[a-z]+$/i.test(str).test(guess)) return "Your guess must only contain letters!"
 
-    const embed = new MessageEmbed()
+    this.embed = this.embed.addField(guess, 
+      `${Array.from(guess).map(letter => `:regional_indicator_${letter}:`)}
+      ${Array.from(guess).map(letter => this.validateLetter(letter))}`)
 
     return embed
   }
